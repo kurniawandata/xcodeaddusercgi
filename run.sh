ed32c13a584fcd0e62a84c71e888a1484eac39e6
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2021-09-25 17:14:48 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,19 @@ echo "Content-type: text/html"
 echo ""
 username=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[1]}' | awk '{split($0,array,"=")} END{print array[2]}'`
 password=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[2]}' | awk '{split($0,array,"=")} END{print array[2]}'`
+menit=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[3]}' | awk '{split($0,array,"=")} END{print array[2]}'`
+cek=`echo "$QUERY_STRING" | awk '{split($0,array,"&")} END{print array[4]}' | awk '{split($0,array,"=")} END{print array[2]}'`
+random=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
+line=$(head -n 1 kode.txt)
+jam=$(date +"%M")
+if [[ "$menit" != "$jam" ]]; then 
+echo "Batas waktu pendaftaran dibatasi waktunya, silahkan diulangi lagi, batas waktu 1 menit atau kurang"
+else
+if [[ "$cek" != "$line" ]]; then 
+echo "Kode keamanan berubah, silahkan refresh dan daftar kembali"
+else
+acak=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')
+echo $acak > kode.txt
 if [[ "${username}" =~ [^a-z0-9] ]]; then
 echo "Username hanya boleh huruf dan angka"
 else
@@ -15,6 +28,7 @@ echo $username:$password | sudo chpasswd
 sudo mkdir /var/www/html/$username
 sudo chown $username.$username /var/www/html/$username
 sudo chmod 701 /var/www/$username
+sudo cp -r /home/data/* /home/$username
 echo ""
 cat <<EOT
 <!DOCTYPE html>
@@ -29,9 +43,9 @@ Password : $password<br />
 <br />
 Server ini ada di kantor PT. Kode Keamanan Indonesia<br />
 <br />
-Untuk akses terminal linux ; http://securityhub.id:8080<br />
+Untuk akses terminal linux ; http://securityhub.id:2086<br />
 Untuk akses untuk folder web server : /var/www/html/$username<br />
-Untuk akses untuk alamat web kamu : http://securityhub.id:2082/$username<br />
+Untuk akses untuk alamat web kamu : http://securityhub.id:2095/$username<br />
 <br />
 https://xcode.or.id<br />
 </body>
@@ -39,6 +53,8 @@ https://xcode.or.id<br />
 EOT
 else
 echo "Akun linux anda masukkan sudah ada pemiliknya"
+fi
+fi
 fi
 fi
 fi
